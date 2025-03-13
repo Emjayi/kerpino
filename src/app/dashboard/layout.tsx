36129f2ae56a7e8bc400680e136cff237a02dca1
Emjayi <mjs32841@gmail.com>
2025-03-13 04:46:27 +0330

    type error fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,6 +13,7 @@ import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "../ui/mode-toggle";
 import { ProfileButton } from "../ui/dashboard/profile-button";
+import { Logo, LogoIcon } from "../ui/kerpino-logo";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const links = [
@@ -75,31 +76,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </div>
     );
 }
-export const Logo = () => {
-    return (
-        <Link
-            href="/"
-            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
-        >
-            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm shrink-0" />
-            <motion.span
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                className="font-medium text-black dark:text-white whitespace-pre"
-            >
-                Kerpino MVP
-            </motion.span>
-        </Link>
-    );
-};
-export const LogoIcon = () => {
-    return (
-        <Link
-            href="/"
-            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
-        >
-            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm shrink-0" />
-        </Link>
-    );
-};
+
 
--- src/app/ui/kerpino-logo.tsx
+++ src/app/ui/kerpino-logo.tsx
@@ -1,5 +1,30 @@
-export default function KarpinoLogo() {
+import { motion } from "motion/react";
+import Link from "next/link";
+
+export const Logo = () => {
     return (
-        <div></div>
+        <Link
+            href="/"
+            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+        >
+            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm shrink-0" />
+            <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="font-medium text-black dark:text-white whitespace-pre"
+            >
+                Kerpino MVP
+            </motion.span>
+        </Link>
     );
-}
+};
+export const LogoIcon = () => {
+    return (
+        <Link
+            href="/"
+            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+        >
+            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm shrink-0" />
+        </Link>
+    );
+};
