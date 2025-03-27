69631e5b98ce5a4c70002f2e1996d147cfb52dde
Emjayi <mjs32841@gmail.com>
2025-03-27 16:31:46 +0330

    login

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,7 +1,24 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import type React from "react"
+import type { Metadata } from "next"
+import { Suspense } from "react"
+import LoginSkeleton from "./skeleton"
+import { WavyBackground } from "../ui/wavy-background"
+
+export const metadata: Metadata = {
+    title: "Login - Kerpino",
+    description: "Login to your Kerpino account",
+}
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
     return (
-        <section>
-            {children}
-        </section>
-    );
+        <WavyBackground className="">
+            <Suspense fallback={<LoginSkeleton />}>
+                {children}
+            </Suspense>
+        </WavyBackground>
+    )
 }
