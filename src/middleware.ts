1f0c180b05a16d26301e6b82983dcddb07e66846
Emjayi <mjs32841@gmail.com>
2025-03-28 16:36:25 +0330

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -100,6 +100,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.svg).*)"],
+    matcher: [],
 }
 
